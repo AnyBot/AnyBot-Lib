4034ec283485fac569d66c2b65627e704c27c066
Christian <git@anymail.net>
2014-06-22 16:34:25 +0200

    improvements

--- src/eu/anynet/java/util/HttpClient.java
+++ src/eu/anynet/java/util/HttpClient.java
@@ -30,6 +30,12 @@ public class HttpClient
 
    private static final int DEFAULT_MAXDOWNLOADINKBYTE = 2048;
 
+   
+   /**
+    * Add default properties to the fluent object
+    * @param r The request
+    * @return The modified request
+    */
    private static Request defaults(Request r)
    {
       return r
@@ -39,45 +45,98 @@ public class HttpClient
          .version(HttpVersion.HTTP_1_1);
    }
 
+   
+   /**
+    * Create GET request
+    * @param url The URL
+    * @return The request
+    */
    public static Request Get(String url)
    {
       return defaults(Request.Get(url));
    }
 
+   
+   /**
+    * Create POST request
+    * @param url The URL
+    * @param form Formdata
+    * @return The request
+    */
    public static Request Post(String url, Form form)
    {
       return defaults(Request.Post(url)).bodyForm(form.build());
    }
 
-   public static String toString(Request request, int maxinkbyte) throws IOException
+   
+   /**
+    * Request to string
+    * @param request The request object
+    * @param maxbyte Max bytes to fetch
+    * @return The response string
+    * @throws IOException 
+    */
+   public static String toString(Request request, int maxbyte) throws IOException
    {
-      return toString(request.execute(), maxinkbyte);
+      return toString(request.execute(), maxbyte);
    }
 
+   
+   /**
+    * Request to string
+    * @param request The request object
+    * @return The response string
+    * @throws IOException 
+    */
    public static String toString(Request request) throws IOException
    {
       return toString(request.execute(), -1);
    }
 
-   public static String toString(Response response, int maxinkbyte) throws IOException
+   
+   /**
+    * Response to String
+    * @param response The response object
+    * @param maxbyte Max bytes to fetch 
+    * @return The string
+    * @throws IOException 
+    */
+   public static String toString(Response response, int maxbyte) throws IOException
    {
       Content content = response.returnContent();
-      return toString(content.asStream(), maxinkbyte);
+      return toString(content.asStream(), maxbyte);
    }
 
-   public static String toString(InputStream stream, int maxinkbyte) throws IOException
+   
+   /**
+    * Read from inputstream and get the result as string
+    * @param stream The stream
+    * @param maxlength Max bytes to fetch, set &lt;1 to disable max length
+    * @return The string
+    * @throws IOException 
+    */
+   public static String toString(InputStream stream, int maxlength) throws IOException
    {
       BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
       Writer resultbuffer = new StringWriter();
 
       char buffer[] = new char[1024];
-      int totalReadSize = 0, currentRead=0;
+      int totalReadSize = 0, currentRead=0, writelength=0;
 
       while ((currentRead = reader.read(buffer,0,buffer.length)) != -1)
       {
          totalReadSize += currentRead;
-         resultbuffer.write(buffer, 0, currentRead);
-         if(maxinkbyte>0 && totalReadSize>=(maxinkbyte*1024))
+         writelength = currentRead;
+         if(maxlength>0 && totalReadSize>maxlength)
+         {
+            writelength = maxlength-totalReadSize;
+         }
+         
+         if(writelength>0)
+         {
+            resultbuffer.write(buffer, 0, writelength);
+         }
+         else
          {
             break;
          }
@@ -87,21 +146,42 @@ public class HttpClient
       return resultbuffer.toString();
    }
 
+   
+   /**
+    * Response to string
+    * @param response The response object
+    * @return The string
+    * @throws IOException 
+    */
    public static String toString(Response response) throws IOException
    {
       return toString(response, -1);
    }
 
+   
+   /**
+    * Request to JSONObject (max 2048 KB)
+    * @param request The request object
+    * @return The JSON Object
+    * @throws IOException 
+    */
    public static JSONObject toJsonObject(Request request) throws IOException
    {
-      String text = toString(request, DEFAULT_MAXDOWNLOADINKBYTE);
+      String text = toString(request, DEFAULT_MAXDOWNLOADINKBYTE*1024);
       JSONObject json = (JSONObject)JSONValue.parse(text);
       return json;
    }
 
+   
+   /**
+    * Request to JSONArray (max 2048 KB)
+    * @param request The request object
+    * @return The JSON Array
+    * @throws IOException 
+    */
    public static JSONArray toJsonArray(Request request) throws IOException
    {
-      String text = toString(request, DEFAULT_MAXDOWNLOADINKBYTE);
+      String text = toString(request, DEFAULT_MAXDOWNLOADINKBYTE*1024);
       JSONArray json = (JSONArray)JSONValue.parse(text);
       return json;
    }
